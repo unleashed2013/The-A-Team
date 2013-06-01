0887a376e467cd47abc45effaeaa5eba2e710fb1
Roger Noble <roger@rogernoble.com>
2013-06-01 20:41:34 +0930

    Added R model output data. Showing results of postcode search.

--- Web Site/public/js/controllers.js
+++ Web Site/public/js/controllers.js
@@ -12,7 +12,42 @@ function PostcodeResultCtrl($scope, webappService, Postcode) {
 	var result = Postcode.GetSLA(
 		webappService.getPostcode(), 
 		function(data){
-			$('#postcode-results').html(JSON.stringify(data));
+			var mapOptions = {
+	          center: new google.maps.LatLng(data.Lattitude, data.Longitude),
+	          zoom: 11,
+	          mapTypeId: google.maps.MapTypeId.ROADMAP
+	        };
+
+	        //create the map
+	        var map = new google.maps.Map(
+	        	document.getElementById('postcode-map'),
+	            mapOptions);
+
+	        //bucket text
+	        var bucketLabel = '',
+	        	backetClass = 'bucket-' + data.Quintile;
+        	switch(data.Quintile){
+        		case 1:
+        			bucketLabel = 'Low';
+        			break;
+    			case 2:
+        			bucketLabel = 'Medium-low';
+        			break;
+    			case 3:
+        			bucketLabel = 'Medium';
+        			break;
+    			case 4:
+        			bucketLabel = 'High';
+        			break;
+    			case 5:
+        			bucketLabel = 'Extreme';
+        			break;
+    			default:
+    				bucketLabel = 'Low';
+        	}
+	        $('#bucket')
+	        	.addClass(backetClass)
+	        	.html(bucketLabel);
 		});
 };
 PostcodeResultCtrl.$inject = ['$scope', 'webappService', 'Postcode'];
